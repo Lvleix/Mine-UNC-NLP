c8088a2c49b99226d94e9a5057654575928cc023
Lv <1144532078@qq.com>
2019-06-25 14:21:25 +0800

    model fix and embedding

--- Net/SemanticNet.py
+++ Net/SemanticNet.py
@@ -4,16 +4,18 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 class NSMN(nn.Module):
-	def __init__(self, d0, d1, d2, d3, hidden_dim, lstm_layers):
+	def __init__(self, d0, d1, d2, d3, hidden_dim = 3, lstm_layers = 1, classify_num = 2, use_gpu = False):
 		super(NSMN, self).__init__()
 		self.embedding_dim = d0
+		#输出层类别数
+		self.classify_num = classify_num
 		# 网络中一些中间量的维度
 		self.dim1 = d1
 		self.dim2 = d2
 		self.dim3 = d3
 		#LSTM中中的隐藏层维度
 		self.hidden_dim = hidden_dim
-		#LSTM中中的隐藏层维度
+		#LSTM层数
 		self.lstm_layers = lstm_layers
 		# encoding层的LSTM
 		self.encoding_lstm = nn.LSTM(d0, d1, batch_first = True, num_layers = lstm_layers)
@@ -22,12 +24,19 @@ class NSMN(nn.Module):
 		#仿射函数
 		self.f_function = nn.Linear(4*d1, d2)
 		self.h_function_l1 = nn.Linear(4*d3, 3)
-		self.h_function_l2 = nn.Linear(3, 2)
+		self.h_function_l2 = nn.Linear(3, classify_num)
+		#配置
+		self.gpu = use_gpu
+		self.loss = nn.CrossEntropyLoss(reduce=True, size_average=True)
+		self.opt = optim.SGD(self.parameters(), lr=0.1)
 		pass
 	
-	def init_hidden(self,dim, batch_size):
+	def init_hidden(self, dim, batch_size):
 		# (num_layers, minibatch_size, hidden_dim)
-		return (torch.zeros(self.lstm_layers, batch_size, dim).cuda(),torch.zeros(self.lstm_layers, batch_size, dim).cuda())
+		if self.gpu:
+			return (torch.zeros(self.lstm_layers, batch_size, dim).cuda(),torch.zeros(self.lstm_layers, batch_size, dim).cuda())
+		else:
+			return (torch.zeros(self.lstm_layers, batch_size, dim),torch.zeros(self.lstm_layers, batch_size, dim))
 	
 	def forward(self, U, V, batch_size):
 		#状态初始化
@@ -62,13 +71,20 @@ class NSMN(nn.Module):
 		m = F.softmax(m, dim=1)
 		return m
 
+	def train_batch(self, batch_u, batch_v, batch_tags, learning_rate = 0.1):
+		batch_size, _, _ =batch_u.size() 
+		
+		self.zero_grad()
+		res = self.forward(batch_u,batch_v,batch_size)
+		loss = self.loss(res, batch_tags)
+		loss.backward()
+		self.opt.step()
+		return
+
 if __name__ == '__main__':
 	tag1 = torch.tensor([1,1,1,1,1,1,1,1,1,1]).cuda()
 	tag2 = torch.tensor([0,0,0,0,0,0,0,0,0,0]).cuda()
-	model = NSMN(5,4,4,3,3,2).cuda()
-
-	loss_function = nn.CrossEntropyLoss(reduce=True, size_average=True)
-	optimizer = optim.SGD(model.parameters(), lr=0.1)
+	model = NSMN(5,4,4,3,hidden_dim = 3, lstm_layers = 2, classify_num = 2, use_gpu = True).cuda()
 
 	u = torch.rand(10,4,5).cuda()
 	v = torch.rand(10,4,5).cuda()
@@ -76,14 +92,8 @@ if __name__ == '__main__':
 	new_u = torch.cat((u,u),0)
 	new_v = torch.cat((u,v),0)
 	tag = torch.cat((tag1,tag2),0)
-	for epoch in range(0):	
-		model.zero_grad()
-		res = model(new_u,new_v,20)
-		loss = loss_function(res, tag)
-		if epoch%20 == 0:
-			print(loss)
-		loss.backward()
-		optimizer.step()
+	for epoch in range(10):	
+		model.train_batch(new_u, new_v, tag)
 
 	res = model(u,v,10)
 	print(res)
